cfa027abb2d3bad3339f35e7e382d37a0bc63e6b
Mike Gehard <mike.gehard@gmail.com>
2017-12-11 16:47:40 -0700

    Initial repo

---
+++ src/test/kotlin/FooTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FooTest {
+    @Test
+    fun testingJUnitSetup() {
+        Assertions.assertEquals(4, 4)
+    }
+}
