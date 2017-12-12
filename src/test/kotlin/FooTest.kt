ff428c69612ced56b094c7344d77d94013694e5f
Mike Gehard <mike.gehard@gmail.com>
2017-12-11 19:25:41 -0700

    Day 1 implementation

--- src/test/kotlin/FooTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-
-class FooTest {
-    @Test
-    fun testingJUnitSetup() {
-        Assertions.assertEquals(4, 4)
-    }
-}
